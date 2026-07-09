002b1b4046266bfef900d20f2be7e158f8ff69dd
Isaias Duran <isaduran644@gmail.com>
2026-07-09 20:05:10 -0300

    Agrego navbar responsive para celulares

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,15 +1,47 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "./Navbar.css";
+
 function Navbar() {
+
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="site-header">
+    <header className="site-header">
+
       <h1>&lt; Isaias Duran &gt;</h1>
 
-      <Link to="/">Inicio</Link>
-      <Link to="/sobre-mi">Sobre mí</Link>
-      <Link to="/Repo">Repositorios</Link>
-      <Link to="/contacto">Contáctame</Link>
-    </div>
+
+      <button 
+        className="menu-icon"
+        onClick={() => setOpen(!open)}
+      >
+        {open ? <FaTimes /> : <FaBars />}
+      </button>
+
+
+      <nav className={open ? "nav-menu active" : "nav-menu"}>
+
+        <Link to="/" onClick={() => setOpen(false)}>
+          Inicio
+        </Link>
+
+        <Link to="/sobre-mi" onClick={() => setOpen(false)}>
+          Sobre mí
+        </Link>
+
+        <Link to="/Repo" onClick={() => setOpen(false)}>
+          Repositorios
+        </Link>
+
+        <Link to="/contacto" onClick={() => setOpen(false)}>
+          Contáctame
+        </Link>
+
+      </nav>
+
+    </header>
   );
 }
 
